3f09e8989d7312139e9fd51c56a1aa9b5b32ed2f
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-19 22:30:36 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html>
+ <head>
+<title>Nepal Zakat Foundation</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="main.php">Nepal Zakat Foundation</a>
+<button class="navbar-toggler" type="button" data-toggle= "collapse" data-target="#navbarSupportedContent" 
+aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Togglenavigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarsupportedcontent">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item active">
+<a class="nav-link" href="index.php">Home <span class="sr-only">
+(Current)</span>
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Our Work</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="about.php">About</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Contact</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="login.php">Login</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="registrationform.php">Sign Up</a>
+</li>
+</ul>
+<form class="form-inline my-2 my-lg-0">
+<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+</div>
+</nav>
+
+
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+<ul class="carousel-indicators">
+<li data-target="#demo" data-slide-to="0" class="active"></li>
+<li data-target="#demo" data-slide-to="1"></li>
+<li data-target="#demo" data-slide-to="2"></li>
+</ul>
+<div class="carousel-inner">
+<div class="carousel-item active">
+<img src="img\zakat1.png" alt="zakat" width="1100" height="500">
+<div class="carousel-caption">
+<h3>Zakat</h3>
+<p>
+“And establish prayer and give zakat, and whatever good you put forward for yourselves  you will find it with Allah.” (2:110, Qur'an)</p>
+</div>
+</div>
+<div class="carousel-item">
+<img src="img\zakat4.png" alt="zakat" width="1100" height="500">
+<div class="carousel-caption">
+<h3> Emergency Releif</h3>
+<p>In the Quran, Allah (swt) says: “Help one another in acts of piety and righteousness.
+And do not assist each other in acts of sinfulness and transgression. And be aware of Allah. 
+Verily, Allah is severe in punishment” (Quran 5:2).</p>
+</div>
+</div>
+<div class="carousel-item">
+<img src="img\zakat3.png" alt="zakat" width="1100" height="500">
+<div class="carousel-caption">
+<h3>Sadaqah Jariya</h3>
+<p>The Qur'an reminds us that there is a “recognised right, for the needy and deprived” over our wealth (70:24-5)</p>
+</div>
+</div>
+</div>
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+<span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+<span class="carousel-control-next-icon"></span>
+</a>
+</div>
+
+
+
+<section class="my-5">
+<div class="py-5">
+<h2 class="text-center">About NZF</h2>
+</div>
+
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img\zakat2.png" class="img-fluid aboutimg">
+</div>
+<div class="col-lg-6 col-md-6 col-12">
+<h2>Your ZAKAT is powerful. We use it as a vehicle for social change. </h2>
+<p>Our life's work has been to channel the spirit of zakat into a global vision for giving. 
+Rest assured that our resources are here to help you give your zakat where it can have the most impact. 
+</p>
+<a href="about.php"> check More </a>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+<section class="my-5">
+<div class="py-5">
+<h2 class="text-center">NZF Aims</h2>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-12">
+
+<h2>Those Who Spend In Charity Will Be Richly Reworded </h2>
+<p>We help Muslims carry out their Zakat 
+duties easily and correctly and deliver 
+these obligatory alms and voluntary 
+Sadaqat charity from all caring people
+diligently, effectually, and with dignity 
+to the poor, the hunger, education sector, 
+health sector and the bereaved of all 
+nation. 
+</p>
+</div>
+
+
+
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img\ourwork.png" class="img-fluid aboutimg">
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="my-5">
+<div class="py-5">
+<h2 class="text-center">NZF Works</h2>
+</div>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+            <div class="card">
+                <img class="card-img-top" src="img\eduction.png" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">Educational Program</h4>
+                    <p class="card-text">We have done computer programing courses .</p>
+                </div>
+            </div>
+        </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img\food.png" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title">Food Distribution Program </h4>
+                            <p class="card-text">We have done food distribution program in this Ramadan month.</p>
+                            
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <div class="card">
+                    <img class="card-img-top" src="img\weekly.png" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title">Islamic Programs</h4>
+                        <p class="card-text">We do islamic programs and quizes weekly.</p>
+                        
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+    <h2 class="text-center">Gallery of NZF</h2>
+</div>
+<div class ="conainer-fluid">
+    <div class ="row">
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal6.png" class ="img-fluid pb-3">
+        </div>
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal1.png" class ="img-fluid pb-3">
+        </div>
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal3.png" class ="img-fluid pb-3">
+        </div>
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal5.png" class ="img-fluid pb-3">
+        </div>
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal4.png" class ="img-fluid pb-3">
+        </div>
+        <div class ="col-lg-4 col-md-4 col-12">
+            <img src="img\gal2.png" class ="img-fluid pb-3">
+        </div>
+    </div>
+</div>
+</section>
+
+<footer class="bg-dark text-white pt-5 pb-4">
+
+	<div class="container text-center text-md-left">
+
+		<div class="row text-center text-md-left">
+
+			<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Nepal Zakat Foundation</h5>
+				<p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          ital consectetur lorem ipsum dolor sit amet adipisicing elit.</p>
+				
+			</div>
+
+			<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products</h5>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> TheProviders</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> Creativity</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> SourceFiles</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> bootstrap 5 alpha</a>
+			</p>
+
+			</div>
+
+			<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">About Zakat</h5>
+			<p>
+				<a href="#https://www.islamic-relief.org/zakat/zakat-calculator/" class="text-white" style="text-decoration: none;">Zakat Calculator</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Zakat" class="text-white" style="text-decoration: none;">Zakat</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Sadaqah" class="text-white" style="text-decoration: none;">Sadaqah</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Islam_and_children" class="text-white" style="text-decoration: none;">Orphan</a>
+			</p>
+			</div>
+
+			<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning" >Contact</h5>
+				<p>
+					<i class="fas fa-home mr-3"></i>Sagartole -2 Kapilvastu municipality taulihawa Nepal
+				</p>
+				<p>
+					<i class="fas fa-envelope mr-3"></i>salamabdul129@gmail.com
+				</p>
+				<p>
+					<i class="fas fa-phone mr-3"></i>+977 9813801266
+				</p>
+				<p>
+					<i class="fas fa-id-card mr-3"></i>Abdul Salam
+				</p>
+			</div>
+			
+		</div>
+
+		<hr class="mb-4">
+
+		<div class="row align-items-center">
+
+			<div class="col-md-7 col-lg-8">
+				<p>	Copyright ©2020 All rights reserved by:
+					<a href="#" style="text-decoration: none;">
+						<strong class="text-warning">Nepal Zakat Foundation</strong>
+					</a></p>
+				
+			</div>
+
+			<div class="col-md-5 col-lg-4">
+				<div class="text-center text-md-right">
+
+					<ul class="list-unstyled list-inline">
+						<li class="list-inline-item">
+							<a href="Fb.com/NepalZakatFoundation" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-github"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="https://www.youtube.com/channel/UCEN4Ps2BYX6HAn1fXLFThOg" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
+						</li>
+					</ul>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+
+	</div>
+	
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
