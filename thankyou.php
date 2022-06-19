a243f4327b5954c4c6feefe0094136beb657831c
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-19 22:45:46 +0300

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+    <title>Thank you</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Nepal Zakat Foundation</a>
+
+    <div class="collapse navbar-collapse" id="navbarsupportedcontent">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item active">
+<a class="nav-link" href="logout.php">LogOut <span class="sr-only">
+(Current)</span>
+</a>
+</li>
+<li class="nav-item ">
+<a class="nav-link" href="list.php">List of Donation <span class="sr-only">
+(Current)</span>
+</a>
+</li>
+<li class="nav-item ">
+<a class="nav-link" href="upadte.php">Update your Donation <span class="sr-only">
+(Current)</span>
+</a>
+</li>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="delete.php">Delete Donation <span class="sr-only">
+(Current)</span>
+</a>
+</li>     
+</nav>
+</br>
+</br>
+ <h1 class="text-center"> Thank you for your donation we will gonna inform you about  payment via Email</h1>
+</br>
+</br>
+<hr>
+
+
+
+ 
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+</body>
+</html>
