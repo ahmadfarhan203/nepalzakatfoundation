a06d6b6bafb0123335d2637679e7ae673ab5a8e5
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-19 22:41:00 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,44 @@
+<html>
+<head><meta charset="utf-8"></head>
+<body>
+<?php
+include("config.php");
+$username= $_POST['username'];
+$name= $_POST['name'];
+$adress= $_POST['adress'];
+$phone= $_POST['phone'];
+$email= $_POST['email'];
+$pass= $_POST['pass'];
+$confirmpass= $_POST['confirmpass'];
+   $pass = hash('sha256', $password); 
+   $confirmpass = hash('sha256', $password); 
+if($pass==$confirmpass){
+$c="INSERT INTO signin". 
+"(username,name,adress,phone,email,password,password2)" . 
+ "VALUES('$username','$name','$adress','$phone','$email','$pass','$confirmpass')";
+$y=mysqli_query($con,$c);
+
+//eger cevap FALSE ise hata yazdiriyoruz.      
+/*if(!$y)
+{
+    echo '<br>Hata:' . mysqli_error($con);
+}
+else
+{
+    echo "Veritabanina eklendi, Kayitlari gormek için";
+}
+*/
+
+echo'<script type ="text/javascript">alert ("Registration Completed")</script>';
+header("location: login.php");
+}
+else{
+    
+    echo'<script type ="text/javascript">alert ("Confirm password should be same of password")</script>';
+    header("location: registrationform.php");
+}
+mysqli_close($con);
+?>
+</body>
+</html>
+
