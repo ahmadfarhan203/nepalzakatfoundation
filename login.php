656588a86a0c2477a0535e0c7fb77662d59d8e70
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-19 22:34:39 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+<link rel="stylesheet" href="css/style.css">
+    <title>Login Page </title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="main.php">Nepal Zakat Foundation</a>
+<button class="navbar-toggler" type="button" data-toggle= "collapse" data-target="#navbarSupportedContent" 
+aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Togglenavigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarsupportedcontent">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item ">
+<a class="nav-link" href="main.php">Home <span class="sr-only">
+(Current)</span>
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="about.php">About</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="contact.php">Contact</a>
+</li>
+<li class="nav-item active">
+<a class="nav-link" href="login.php">Login</a>
+
+</li>
+<li class="nav-item">
+<a class="nav-link" href="registrationform.php">Sign Up</a>
+</li>
+</ul>
+<form class="form-inline my-2 my-lg-0">
+<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+</div>
+</nav>
+</br>
+</br>
+</br>
+</br>
+    <div class="container">
+        <h1>Login</h1>
+        <div class="row">
+            <div class ="col-lg-6">
+                <form action="validation.php" method="post">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control">
+                         </div>
+                     <div class="form-group">
+                        <label>Password</label>
+                        <input type="Password" name="password" class="form-control">
+                      </div>
+                      <button type="submit"class="btn btn-primary">Login</button>
+                      <a href="registrationform.php" class="btn btn-primary" role="button">Sign Up</a>
+
+                </form>
+            </div>
+        </div>
+    </div>
+    </br>
+    </br>
+    </br>
+    </br>
+
+    <footer class="bg-dark text-white pt-5 pb-4">
+
+<div class="container text-center text-md-left">
+
+    <div class="row text-center text-md-left">
+
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Nepal Zakat Foundation</h5>
+            <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+      ital consectetur lorem ipsum dolor sit amet adipisicing elit.</p>
+            
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products</h5>
+        <p>
+            <a href="#" class="text-white" style="text-decoration: none;"> TheProviders</a>
+        </p>
+        <p>
+            <a href="#" class="text-white" style="text-decoration: none;"> Creativity</a>
+        </p>
+        <p>
+            <a href="#" class="text-white" style="text-decoration: none;"> SourceFiles</a>
+        </p>
+        <p>
+            <a href="#" class="text-white" style="text-decoration: none;"> bootstrap 5 alpha</a>
+        </p>
+
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">About Zakat</h5>
+        <p>
+            <a href="#https://www.islamic-relief.org/zakat/zakat-calculator/" class="text-white" style="text-decoration: none;">Zakat Calculator</a>
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Zakat" class="text-white" style="text-decoration: none;">Zakat</a>
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Sadaqah" class="text-white" style="text-decoration: none;">Sadaqah</a>
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Islam_and_children" class="text-white" style="text-decoration: none;">Orphan</a>
+        </p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-warning" >Contact</h5>
+            <p>
+                <i class="fas fa-home mr-3"></i>Sagartole -2 Kapilvastu municipality taulihawa Nepal
+            </p>
+            <p>
+                <i class="fas fa-envelope mr-3"></i>salamabdul129@gmail.com
+            </p>
+            <p>
+                <i class="fas fa-phone mr-3"></i>+977 9813801266
+            </p>
+            <p>
+                <i class="fas fa-id-card mr-3"></i>Abdul Salam
+            </p>
+        </div>
+        
+    </div>
+
+    <hr class="mb-4">
+
+    <div class="row align-items-center">
+
+        <div class="col-md-7 col-lg-8">
+            <p>	Copyright ©2020 All rights reserved by:
+                <a href="#" style="text-decoration: none;">
+                    <strong class="text-warning">Nepal Zakat Foundation</strong>
+                </a></p>
+            
+        </div>
+
+        <div class="col-md-5 col-lg-4">
+            <div class="text-center text-md-right">
+
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item">
+                        <a href="Fb.com/NepalZakatFoundation" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-github"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://www.youtube.com/channel/UCEN4Ps2BYX6HAn1fXLFThOg" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+
+</div>
+
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
