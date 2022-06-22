ea367029344b15a360a6a3613f70e00d09d4320b
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 02:41:52 +0300

    Update deleteform.php

--- deleteform.php
+++ deleteform.php
@@ -10,16 +10,16 @@ if(!$ans )
     echo '<br>Hata:' . mysqli_error($con);
 }
 echo "<table border=1>";
-echo "<tr><th>donor_id</th><th>zakat</th><th>sadaqah</th><th>releif</th><th>orphan</th><th>amount</th></tr>";
+echo "<tr><th>donorID</th><th>zakat</th><th>sadaqah</th><th>releif</th><th>orphan</th><th>amount</th></tr>";
 while($com=mysqli_fetch_array($ans))
 {
-    echo "<tr><td>".$com['donor_id']."</td>";
+    echo "<tr><td>".$com['donorID']."</td>";
     echo "<tr><td>".$com['zakat']."</td>";
     echo "<td>".$com['sadaqah']."</td>";
     echo "<td>".$com['releif']."</td>";
     echo "<td>".$com['orphan']."</td>";
     echo "<td>".$com['amount']."</td></tr>";   
-  echo "<td><a href=delete.php?id=".$com['donor_id'].">Sil</a></td></tr>";    
+  echo "<td><a href=delete.php?id=".$com['donorID'].">Delete</a></td></tr>";    
 }
 echo "</table>";
 mysqli_close($con);
