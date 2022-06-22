da609cdc721918f664976e2a6e8f71c2799a746a
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 02:57:55 +0300

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -4,38 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
-
-    <title>Thank you</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Nepal Zakat Foundation</a>
+    <div class="container-fluid">
+         <a class="navbar-brand" href="index.php">Nepal Zakat Foundation</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <div class="collapse navbar-collapse" id="navbarsupportedcontent">
-<ul class="navbar-nav ml-auto">
-<li class="nav-item active">
-<a class="nav-link" href="logout.php">LogOut <span class="sr-only">
-(Current)</span>
-</a>
-</li>
-<li class="nav-item ">
-<a class="nav-link" href="list.php">List of Donation <span class="sr-only">
-(Current)</span>
-</a>
-</li>
-<li class="nav-item ">
-<a class="nav-link" href="upadte.php">Update your Donation <span class="sr-only">
-(Current)</span>
-</a>
-</li>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="delete.php">Delete Donation <span class="sr-only">
-(Current)</span>
-</a>
-</li>     
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item ">
+                    <a class="nav-link" href="logout.php">LogOut <span class="sr-only">
+                    (Current)</span>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="list.php">List of Donation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="upadtelist.php">Update Donation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="deleteform.php">Delete Donation</a>
+                </li>
+            </ul>
+ 
+        </div>
+     </div>
 </nav>
 </br>
 </br>
@@ -50,7 +48,6 @@
 
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
