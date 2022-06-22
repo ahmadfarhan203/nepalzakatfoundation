5bf34c8966b99d5cb637a6dc7b18021673567b60
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 02:48:19 +0300

    Update register.php

--- register.php
+++ register.php
@@ -10,24 +10,14 @@ $phone= $_POST['phone'];
 $email= $_POST['email'];
 $pass= $_POST['pass'];
 $confirmpass= $_POST['confirmpass'];
-   $pass = hash('sha256', $pass); 
-   $confirmpass = hash('sha256', $confirmpass); 
+$pass4 = hash('sha256', $pass); 
+$pass1 = hash('sha256', $confirmpass); 
 if($pass==$confirmpass){
 $c="INSERT INTO signin". 
 "(username,name,adress,phone,email,password,password2)" . 
- "VALUES('$username','$name','$adress','$phone','$email','$pass','$confirmpass')";
+ "VALUES('$username','$name','$adress','$phone','$email','$pass4','$pass1')";
 $y=mysqli_query($con,$c);
 
-//eger cevap FALSE ise hata yazdiriyoruz.      
-/*if(!$y)
-{
-    echo '<br>Hata:' . mysqli_error($con);
-}
-else
-{
-    echo "Veritabanina eklendi, Kayitlari gormek için";
-}
-*/
 
 echo'<script type ="text/javascript">alert ("Registration Completed")</script>';
 header("location: login.php");
@@ -35,9 +25,10 @@ header("location: login.php");
 else{
     
     echo'<script type ="text/javascript">alert ("Confirm password should be same of password")</script>';
-    header("location: registrationform.php");
+    echo"<script> window.location.href= 'registrationform.php'</script>";
 }
 mysqli_close($con);
+
 ?>
 </body>
 </html>
