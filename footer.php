d6b7d258cec19a142d2b1bf52fb52693e4b1d9a4
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 03:12:37 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+ <head>
+<title>Nepal Zakat Foundation</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+</head>
+<body>
+
+
+<footer class="bg-dark text-white pt-5 pb-4">
+
+	<div class="container text-center text-md-left">
+
+		<div class="row text-center text-md-left">
+
+			<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Nepal Zakat Foundation</h5>
+				<p>Zakat is a charity God obligates Muslims to pay yearly on
+their money and property. Its payment is made to the poor,
+vulnerable, and deserving as their divinely established right.
+The Prophet Muhammad, on him be peace, established Zakat
+as the third of the five pillars that Islam is built on</p>
+				
+			</div>
+
+			<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Five Pillar Of Islam</h5>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> Profession of Faith (shahada)</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> Salah</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;"> Zakat</a>
+			</p>
+			<p>
+				<a href="#" class="text-white" style="text-decoration: none;">Fast</a>
+			</p>
+            <p>
+				<a href="#" class="text-white" style="text-decoration: none;">Pilgrimage</a>
+			</p>
+
+			</div>
+
+			<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">About Zakat</h5>
+			<p>
+				<a href="#https://www.islamic-relief.org/zakat/zakat-calculator/" class="text-white" style="text-decoration: none;">Zakat Calculator</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Zakat" class="text-white" style="text-decoration: none;">Zakat</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Sadaqah" class="text-white" style="text-decoration: none;">Sadaqah</a>
+			</p>
+			<p>
+				<a href="https://en.wikipedia.org/wiki/Islam_and_children" class="text-white" style="text-decoration: none;">Orphan</a>
+			</p>
+			</div>
+
+			<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning" >Contact</h5>
+				<p>
+					<i class="fas fa-home mr-3"></i>Sagartole -2 Kapilvastu municipality taulihawa Nepal
+				</p>
+				<p>
+					<i class="fas fa-envelope mr-3"></i>salamabdul129@gmail.com
+				</p>
+				<p>
+					<i class="fas fa-phone mr-3"></i>+977 9813801266
+				</p>
+				<p>
+					<i class="fas fa-id-card mr-3"></i>Abdul Salam
+				</p>
+			</div>
+			
+		</div>
+
+		<hr class="mb-4">
+
+		<div class="row align-items-center">
+
+			<div class="col-md-7 col-lg-8">
+				<p>	Copyright ©2020 All rights reserved by:
+					<a href="#" style="text-decoration: none;">
+						<strong class="text-warning">Nepal Zakat Foundation</strong>
+					</a></p>
+				
+			</div>
+
+			<div class="col-md-5 col-lg-4">
+				<div class="text-center text-md-right">
+
+					<ul class="list-unstyled list-inline">
+						<li class="list-inline-item">
+							<a href="https://www.facebook.com/NepalZakatFoundation" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="https://github.com/ahmadfarhan203/nepalzakatfoundation" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-github"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="https://www.youtube.com/channel/UCEN4Ps2BYX6HAn1fXLFThOg" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
+						</li>
+					</ul>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+
+	</div>
+	
+</footer>
+
+</body>
+</html>
