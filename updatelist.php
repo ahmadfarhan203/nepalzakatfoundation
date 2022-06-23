cc0efb120a5fb9b269a214e9e93f683b60431da0
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 03:08:55 +0300

    Update updatelist.php

--- updatelist.php
+++ updatelist.php
@@ -1,18 +1,14 @@
 <?php
-//mysql$con kodunu ekliyoruz
-include("config.php");
 
-//degiskenleri formdan aliyoruz
+include("config.php");
 extract($_POST);
 
-//sorguyu hazirliyoruz
 $sql ="UPDATE donations ".
       "SET zakat='$zakat',sadaqah='$sadaqah',releif='$releif',orphan='$orphan',amount='$amount' ".
-      "WHERE donor_id=".$_GET['donor_id'];
-//sorguyu veritabanina gönderiyoruz.
-$ans = mysqli_query( $con,$sql);
+      "WHERE donor_id=".$_GET['donorID'];
 
-//eger ans FALSE ise hata yazdiriyoruz.      
+$ans = mysqli_query( $con,$sql);
+      
 if(!$ans){
     echo '<br>Hata:' . mysqli_error($con);
 }
@@ -21,6 +17,5 @@ else{
     echo " <br><a href='list.php'> Listele</a>\n";
 }
 
-//veritabani$consini kapatiyoruz.
 mysqli_close($con);
 ?>
