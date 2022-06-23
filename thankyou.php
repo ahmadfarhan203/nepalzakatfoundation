55dee88ace597846177e71fa83caca8fa4c75e75
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-23 03:04:20 +0300

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -16,12 +15,12 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto ">
-                <li class="nav-item ">
+                <li class="nav-item active ">
                     <a class="nav-link" href="logout.php">LogOut <span class="sr-only">
                     (Current)</span>
                     </a>
                 </li>
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="list.php">List of Donation</a>
                 </li>
                 <li class="nav-item">
