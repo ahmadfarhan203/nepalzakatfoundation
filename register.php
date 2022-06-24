4758f97e326a4dffac6541f351247aa03f6964a3
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-24 19:39:21 +0300

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,14 @@
 <head><meta charset="utf-8"></head>
 <body>
 <?php
-include("config.php");
+if(isset($_POST['submit'])){
+include "config.php";
+if(!$con)
+{
+    echo "connection failed";
+}
+else{
+    
 $username= $_POST['username'];
 $name= $_POST['name'];
 $adress= $_POST['adress'];
@@ -12,15 +19,19 @@ $pass= $_POST['pass'];
 $confirmpass= $_POST['confirmpass'];
 $pass4 = hash('sha256', $pass); 
 $pass1 = hash('sha256', $confirmpass); 
+
+
 if($pass==$confirmpass){
-$c="INSERT INTO signin". 
-"(username,name,adress,phone,email,password,password2)" . 
- "VALUES('$username','$name','$adress','$phone','$email','$pass4','$pass1')";
-$y=mysqli_query($con,$c);
+$c = "INSERT INTO signin (username,name,adress,phone,email,paassword,password2) 
+     VALUES ('{$username}','{$name}','{$adress}','{$phone}','{$email}','{$pass4}','{$pass1}')";
 
-
+  if(mysqli_query($con,$c)){
 echo'<script type ="text/javascript">alert ("Registration Completed")</script>';
-header("location: login.php");
+include "login.php";
+}
+else{
+    echo"query failed";
+}
 }
 else{
     
@@ -28,8 +39,8 @@ else{
     echo"<script> window.location.href= 'registrationform.php'</script>";
 }
 mysqli_close($con);
-
+}
+}
 ?>
 </body>
 </html>
-
