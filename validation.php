5b79e783980582e9e99c5ba21dea0b44c50e3494
Farhan Ahmad <76876116+ahmadfarhan203@users.noreply.github.com>
2022-06-24 19:40:05 +0300

    Update validation.php

--- validation.php
+++ validation.php
@@ -6,9 +6,7 @@ include 'config.php';
 $username= $_POST['user'];
 $pass= $_POST['password'];
 $pass2 = hash('sha256', $pass); 
-$s = "SELECT * FROM signin WHERE username='$username' && password='$pass2'";
-
-
+$s = "SELECT * FROM signin WHERE username='$username' && paassword='$pass2'";
 
 $answer=mysqli_query($con,$s);
 
@@ -20,13 +18,13 @@ else{
     $row = mysqli_fetch_array($answer, MYSQLI_ASSOC);  
     $num=mysqli_num_rows($answer);
 if($num==1){
-    
-        $_SESSION['username']=$username;
-        header('location:donationform.php');
+       session_start();
+       $_SESSION['username']=$username;
+       include 'donationform.php';
 }
 else{
     echo'<script type ="text/javascript">alert ("Username or password is wrong")</script>';
-    header('location:login.php');   
+    include 'login.php'; 
 }
 }
 ?>
